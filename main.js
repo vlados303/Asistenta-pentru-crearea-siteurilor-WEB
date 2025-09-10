4446001b5b417f82c5179019f14a69cd44604119
vlados303 <vladislavluca63@gmail.com>
2025-09-10 14:42:03 +0300

    Primul commit

---
+++ main.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const an = document.getElementById('an');
+  if (an) an.textContent = new Date().getFullYear();
+  console.log('Ecomarket: laborator HTML only încărcat.');
+});
